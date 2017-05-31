37d85893ecc503c8f43be33887d7726073598f60
Diego Aguilar and Mick Sexton <mick@>
2017-05-31 13:31:05 -0700

    add required functionality for cat and dog to meow and bark at each other through javascript

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $("button#cat-button").click(function() {
+    $("ul#dog-list").prepend("<li>Bark!</li>");
+    $("ul#dog-list").children("li").first().click(function() {
+      $(this).remove();
+    });
+  });
+  $("button#dog-button").click(function() {
+    $("ul#cat-list").prepend("<li>Meow!!</li>");
+    $("ul#cat-list").children("li").first().click(function() {
+      $(this).remove();
+    });
+  });
+});
