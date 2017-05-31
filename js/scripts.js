97261875a397ce11e646147f91f904a97f300824
Diego Aguilar and Mick Sexton <mick@>
2017-05-31 14:31:28 -0700

    add images directly thru js with .after function, do not have a way to get rid of them like the list items

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,21 @@
 $(document).ready(function() {
   $("button#cat-button").click(function() {
     $("ul#dog-list").prepend("<li>Bark!</li>");
+    $("ul#dog-list").after("<img src='img/dog-bark.jpg'>");
+
+/*
+    $("img src="img/dog-bark.jpg"").click(function() {
+      $(this).remove();
+    });
+*/
+
     $("ul#dog-list").children("li").first().click(function() {
       $(this).remove();
     });
   });
   $("button#dog-button").click(function() {
     $("ul#cat-list").prepend("<li>Meow!!</li>");
+    $("ul#cat-list").after("<img src='img/cat-meow.jpg'>");
     $("ul#cat-list").children("li").first().click(function() {
       $(this).remove();
     });
